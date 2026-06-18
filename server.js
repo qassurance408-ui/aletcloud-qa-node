04f416652745119ab41e63b995d517f2a3660879
qassurance408-ui <qassurance408@gmail.com>
2026-06-18 10:13:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
